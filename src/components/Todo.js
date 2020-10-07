1315b2eee097d2ef7642a057b0889b051abb3d39
thureintun234 <thurein12456@gmail.com>
2020-10-07 13:33:13 +0630

    final part

---
+++ src/components/Todo.js
@@ -0,0 +1,40 @@
+import React,{useState} from 'react'
+import TodoForm from './TodoForm'
+import {FaEdit} from 'react-icons/fa'
+import {AiOutlineCloseSquare} from 'react-icons/ai'
+
+function Todo({todos,completeTodo,removeTodo,updateTodo}) {
+    const [edit,setEdit] = useState({
+        id:null,
+        value:''
+    })
+
+    const submitUpdate = value => {
+        updateTodo(edit.id, value)
+        setEdit({
+            id:null,
+            value:''
+        })
+    }
+
+    if(edit.id) {
+        return <TodoForm edit={edit} onSubmit={submitUpdate} />
+    }
+
+    return todos.map((todo,index) => (
+        <div className={todo.isComplete ? 'todo-row complete' : 'todo-row'}
+            key={index}
+        >
+            <div key={todo.id} onClick={() => completeTodo(todo.id)}>
+                {todo.text}
+            </div>
+            <div className="icons">
+                <AiOutlineCloseSquare onClick={() => removeTodo(todo.id)} className="delete-icon" />
+                <FaEdit onClick={() => setEdit({id:todo.id, value: todo.text})} className="edit-icon" />
+            </div>
+
+        </div>
+    ))
+}
+
+export default Todo
